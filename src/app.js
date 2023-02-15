7de5435e9b3597a0cc6f99fea008347828d671b4
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-02-15 18:53:08 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const { Server } = require('socket.io')
+const routerViews = require('./routes/views.routes')
+
+const app = express()
+const httpServer = app.listen(8080, () => console.log('Server Up!'))
+const io = new Server(httpServer)
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'))
+app.use('/', routerViews)
+
+let messages = []
+
+io.on('connection', socket => {
+    console.log('New client connected!')
+    socket.on('message', data => {
+        messages.push(data)
+        io.emit('logs', messages)
+    })
+})
